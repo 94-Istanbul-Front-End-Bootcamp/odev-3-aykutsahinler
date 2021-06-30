404f56df009f001cd3704a623e2ae000c7a573f9
aykutsahinler <aykut-sahinler@hotmail.com>
2021-06-30 17:36:14 +0300

    get needed elements in .js file

---
+++ index.js
@@ -0,0 +1,20 @@
+// Get Elements
+
+let userName = document.getElementById("userName");
+let password = document.getElementById("password");
+let eMail = document.getElementById("eMail");
+let checkBox = document.getElementById("checkBox");
+let submitButton = document.getElementById("submitButton");
+let inputContainer = document.querySelectorAll(".input-container");
+
+// Event Listeners
+
+submitButton.addEventListener("click", submit);
+
+function isOkey() {
+
+}
+
+function submit() {
+    isOkey();
+}
